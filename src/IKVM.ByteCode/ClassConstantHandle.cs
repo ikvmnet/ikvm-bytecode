91ce97187418351486665df6a091a9d7b0a5c4ed
Jerome Haltom <jhaltom@alethic.solutions>
2024-07-31 18:08:06 -0500

    Add nil.

--- src/IKVM.ByteCode/ClassConstantHandle.cs
+++ src/IKVM.ByteCode/ClassConstantHandle.cs
@@ -4,11 +4,6 @@
     public readonly record struct ClassConstantHandle(ushort Index)
     {
 
-        /// <summary>
-        /// Gets the zero instance.
-        /// </summary>
-        public static readonly ClassConstantHandle Nil = new(0);
-
         public static explicit operator ClassConstantHandle(Handle handle)
         {
             return new ClassConstantHandle(handle.Index);
@@ -28,6 +23,11 @@
         {
             return new ConstantHandle(handle.Index);
         }
+
+        /// <summary>
+        /// Gets the nil instance.
+        /// </summary>
+        public static readonly ClassConstantHandle Nil = new(0);
 
         /// <summary>
         /// Gets whether or not this represents the nil instance.
