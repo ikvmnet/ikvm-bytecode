2bf63e87fabf036ee9b31f5b821e3c80924804d8
Jerome Haltom <jhaltom@alethic.solutions>
2024-07-31 20:08:29 -0500

    Name Constant value Value.

--- src/IKVM.ByteCode/Parsing/ConstantValueAttributeRecord.cs
+++ src/IKVM.ByteCode/Parsing/ConstantValueAttributeRecord.cs
@@ -1,17 +1,17 @@
 ﻿namespace IKVM.ByteCode.Parsing
 {
 
-    public sealed record ConstantValueAttributeRecord(ConstantHandle Handle) : AttributeRecord
+    public sealed record ConstantValueAttributeRecord(ConstantHandle Value) : AttributeRecord
     {
 
         public static bool TryReadConstantValueAttribute(ref ClassFormatReader reader, out AttributeRecord attribute)
         {
             attribute = null;
 
-            if (reader.TryReadU2(out ushort handleIndex) == false)
+            if (reader.TryReadU2(out ushort valueIndex) == false)
                 return false;
 
-            attribute = new ConstantValueAttributeRecord(new ConstantHandle(handleIndex));
+            attribute = new ConstantValueAttributeRecord(new ConstantHandle(valueIndex));
             return true;
         }
 
