b1efab12a976f09f052b53f39aa01595d233d721
Jerome Haltom <jhaltom@alethic.solutions>
2025-03-11 20:46:46 -0500

    Change version.

--- src/IKVM.ByteCode/ModuleOpensFlag.cs
+++ src/IKVM.ByteCode/ModuleOpensFlag.cs
@@ -11,12 +11,12 @@ namespace IKVM.ByteCode
     {
 
         /// <summary>
-        /// The open package was not explicitly or implicitly declared in the source of the module declaration.
+        /// Indicates that this opening was not explicitly or implicitly declared in the source of the module declaration.
         /// </summary>
         Synthetic = AccessFlag.Synthetic,
 
         /// <summary>
-        /// The open package was implicitly declared in the source of the module declaration.
+        /// Indicates that this opening was implicitly declared in the source of the module declaration.
         /// </summary>
         Mandated = AccessFlag.Mandated,
 
